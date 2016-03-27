785320af010b79bd36f57f16531c80f816294aa3
Sonia7 <soniaverma89@rocketmail.com>
2016-03-26 23:12:36 -0400

    commit

---
+++ Assets/_Scripts/PlayerCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+    // Private INSTANCE VARIABLES
+
+
+    // PUBLIC INSTANCE VARIABLES
+    public GameController gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize the audioSources array
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("apple"))
+        {
+           
+           this.gameController.ScoreValue += 100;
+        }
+     
+    }// used when player collides with apple
+
+}
